40723b85874d12a4c9e9d30f49148f1d48199d81
minkikim98 <minkikim98@gmail.com>
2021-05-05 10:53:26 -0700

    started setting up frontend main page with navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
+import MainPage from "./components/MainPage";
 // import { authenticate } from "./services/auth";
 import { authenticate } from "./store/session";
 
@@ -28,7 +29,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      {/* <NavBar /> */}
       <Switch>
         <Route path="/login" exact={true}>
           <LoginForm />
@@ -42,9 +43,9 @@ function App() {
         <ProtectedRoute path="/users/:userId" exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path="/" exact={true}>
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+        <Route path="/" exact={true}>
+          <MainPage />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,26 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar">
+      <div className="navbar-left">
+        <button>Toggle Menu</button>
+        <button>Delivery Time</button>
+        <div> to </div>
+        <button>Delivery Address</button>
+      </div>
+      <div className="navbar-center">
+        <Link to="/">
+          Courier
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <button>Cart</button>
+      </div>
+      
+    </div>
   );
 }
 
