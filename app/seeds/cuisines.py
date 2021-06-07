2f8b79015780e4c3a282954680f52e2f7ee54516
minkikim98 <minkikim98@gmail.com>
2021-06-07 10:08:52 -0700

    fixed image stretching, changed seeding image urls

--- app/seeds/cuisines.py
+++ app/seeds/cuisines.py
@@ -3,43 +3,43 @@ from app.models import db, Cuisine
 
 def seed_cuisines():
 
-    fast = Cuisine(name='Fast Food', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Fast+Food.jpg")
+    fast = Cuisine(name='Fast Food', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Fast%2BFood.jpeg")
     db.session.add(fast)
 
-    asian = Cuisine(name='Asian', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Asian.jpg")
+    asian = Cuisine(name='Asian', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Asian.jpeg")
     db.session.add(asian)
     
-    burgers = Cuisine(name='Burgers', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Burgers.jpg")
+    burgers = Cuisine(name='Burgers', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Burgers.jpeg")
     db.session.add(burgers)
 
-    chinese = Cuisine(name='Chinese', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Chinese.jpg")
+    chinese = Cuisine(name='Chinese', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Chinese.jpeg")
     db.session.add(chinese)
 
-    healthy = Cuisine(name='Healthy', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Healthy.jpg")
+    healthy = Cuisine(name='Healthy', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Healthy.jpeg")
     db.session.add(healthy)
 
-    breakfast = Cuisine(name='Breakfast', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Breakfast.jpg")
+    breakfast = Cuisine(name='Breakfast', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Breakfast.jpeg")
     db.session.add(breakfast)
 
-    thai = Cuisine(name='Thai', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Thai.jpg")
+    thai = Cuisine(name='Thai', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Thai.jpeg")
     db.session.add(thai)
 
-    chicken = Cuisine(name='Chicken', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Chicken.jpg")
+    chicken = Cuisine(name='Chicken', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Chicken.jpeg")
     db.session.add(chicken)
 
-    mexican = Cuisine(name='Mexican', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Mexican.jpg")
+    mexican = Cuisine(name='Mexican', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Mexican.jpeg")
     db.session.add(mexican)
 
-    japanese = Cuisine(name='Japanese', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Japanese.jpg")
+    japanese = Cuisine(name='Japanese', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Japanese.jpeg")
     db.session.add(japanese)
 
-    italian = Cuisine(name='Italian', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Italian.jpg")
+    italian = Cuisine(name='Italian', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Italian.jpeg")
     db.session.add(italian)
 
-    pizza = Cuisine(name='Pizza', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Pizza.jpg")
+    pizza = Cuisine(name='Pizza', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Pizza.jpeg")
     db.session.add(pizza)
 
-    # convenience = Cuisine(name='Convenience', image_url="https://dannydash-seeds.s3-us-west-1.amazonaws.com/Convenience.jpg")
+    # convenience = Cuisine(name='Convenience', image_url="https://courier-images.s3-us-west-1.amazonaws.com/Convenience.jpeg")
     # db.session.add(convenience)
 
 
