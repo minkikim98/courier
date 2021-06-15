d916c784228c43eba47c900fb5cbb31d5760ebef
minkikim98 <minkikim98@gmail.com>
2021-06-15 15:48:18 -0700

    structured flexbox elements for checkout

--- react-app/src/components/checkout/Checkout.js
+++ react-app/src/components/checkout/Checkout.js
@@ -10,7 +10,16 @@ const Checkout = () => {
         <>
             <SimpleNavBar />
             <div className="checkout-body">
-                <div className="checkout-main">This is the checkout page.</div>
+                <div className="checkout-main__container">
+                    <div className="checkout-main">
+                        <div className="checkout-row">Your Delivery Details</div>
+                        <div>Time</div>
+                        <div>Address</div>
+                        <div>Contact</div>
+                        <div>Payment</div>
+                        <div>Summary</div>
+                    </div>
+                </div>
                 <div className="checkout-right">This will be the right sidebar for the checkout page.</div>
             </div>
             <Footer />
